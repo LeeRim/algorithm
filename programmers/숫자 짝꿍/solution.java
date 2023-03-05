42723cb03bd10a6cdc858a07330ccec18f294c43
LeeRim <yrim113@gmail.com>
2023-03-05 22:57:42 +0900

    uploaded at 2023. 3. 5. 오후 10:57:42

---
+++ programmers/숫자 짝꿍/solution.java
@@ -0,0 +1,44 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/131128
+
+class Solution {
+    public String solution(String X, String Y) {
+
+        int[] xNums = countNum(X);
+        int[] yNums = countNum(Y);
+
+        int[] commons = new int[10];
+        for (int i = 0; i < 10; i++) {
+            commons[i] = Math.min(xNums[i], yNums[i]);
+        }
+//        System.out.println(Arrays.toString(commons));
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 9; i >= 0; i--) {
+            for (int j = 0; j < commons[i]; j++) {
+                sb.append(i);
+            }
+        }
+//        System.out.println(sb);
+
+        if (sb.length() == 0) {
+            return "-1";
+        }
+        if (sb.charAt(0) == '0') {
+            return "0";
+        }
+        
+        return sb.toString();
+    }
+
+    public int[] countNum(String str) {
+        int[] nums = new int[10];
+
+        int index;
+        for (int i = 0; i < str.length(); i++) {
+            index = str.charAt(i) - '0';
+            nums[index]++;
+        }
+
+        return nums;
+    }
+}
