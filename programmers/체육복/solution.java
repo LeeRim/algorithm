4d1f342aab6b0a04117308ea9bfc88e75e786269
LeeRim <yrim113@gmail.com>
2023-03-05 20:47:08 +0900

    uploaded at 2023. 3. 5. 오후 8:47:08

---
+++ programmers/체육복/solution.java
@@ -0,0 +1,27 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42862
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        Set<Integer> reserves = Arrays.stream(reserve).boxed().sorted().collect(Collectors.toSet());
+        int[] lostAll = Arrays.stream(lost).sorted().filter(l -> {
+            if (reserves.remove(l)) {
+                return false;
+            }
+            return true;
+        }).toArray();
+
+        long lostCnt = Arrays.stream(lostAll).filter(l -> {
+            if (reserves.remove(l-1) || reserves.remove(l+1)) {
+                return false;
+            }
+            return true;
+        }).count();
+
+        System.out.println(lostCnt);
+        int answer = n - (int) lostCnt;
+        return answer;
+    }
+}
