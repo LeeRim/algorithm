fa7c12b74fc7d10c241e6e487fc0f565dcd3939c
LeeRim <yrim113@gmail.com>
2023-03-05 21:18:03 +0900

    updated at 2023. 3. 5. 오후 9:18:03

--- programmers/체육복/solution.java
+++ programmers/체육복/solution.java
@@ -1,27 +1,52 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42862
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        Set<Integer> reserves = Arrays.stream(reserve).boxed().sorted().collect(Collectors.toSet());
-        int[] lostAll = Arrays.stream(lost).sorted().filter(l -> {
-            if (reserves.remove(l)) {
-                return false;
-            }
-            return true;
-        }).toArray();
 
-        long lostCnt = Arrays.stream(lostAll).filter(l -> {
-            if (reserves.remove(l-1) || reserves.remove(l+1)) {
-                return false;
-            }
-            return true;
-        }).count();
+        Set<Integer> lostList = Arrays.stream(lost)
+                .boxed()
+                .collect(Collectors.toSet());
 
-        System.out.println(lostCnt);
-        int answer = n - (int) lostCnt;
+        for (int i = 0; i < reserve.length; i++) {
+            if (lostList.contains(reserve[i])) {
+                lostList.remove(reserve[i]);
+                reserve[i] = -1;
+                continue;
+            }
+            if (lostList.contains(reserve[i] - 1) && lostList.contains(reserve[i] + 1)) {
+                continue;
+            }
+            if (lostList.contains(reserve[i] - 1)) {
+                lostList.remove(reserve[i] - 1);
+                reserve[i] = -1;
+                continue;
+            }
+            if (lostList.contains(reserve[i] + 1)) {
+                lostList.remove(reserve[i] + 1);
+                reserve[i] = -1;
+            }
+        }
+
+        for (int i = 0; i < reserve.length; i++) {
+            if (reserve[i] == -1) {
+                continue;
+            }
+            if (lostList.contains(reserve[i] - 1)) {
+                lostList.remove(reserve[i] - 1);
+                reserve[i] = -1;
+                continue;
+            }
+            if (lostList.contains(reserve[i] + 1)) {
+                lostList.remove(reserve[i] + 1);
+                reserve[i] = -1;
+            }
+        }
+
+        int answer = n - lostList.size();
         return answer;
     }
 }
