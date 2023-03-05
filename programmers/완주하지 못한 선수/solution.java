8c0a1ff30e42fa130374fa7471804754beec8389
LeeRim <yrim113@gmail.com>
2023-03-05 23:33:17 +0900

    uploaded at 2023. 3. 5. 오후 11:33:16

---
+++ programmers/완주하지 못한 선수/solution.java
@@ -0,0 +1,23 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42576
+
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        LinkedList<String> partList = new LinkedList<>();
+        for (int i = 0; i < participant.length; i++) {
+            partList.add(participant[i]);
+        }
+
+        partList.sort(Comparator.naturalOrder());
+        Arrays.sort(completion);
+        for (int i = 0; i < completion.length; i++) {
+            if (partList.contains(completion[i])) {
+                partList.remove(completion[i]);
+            }
+        }
+
+//        System.out.println(partList);
+        return partList.get(0);
+    }
+}
