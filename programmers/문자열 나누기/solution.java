a5daa58b71e347645337cb2e091231e9b8266c26
LeeRim <yrim113@gmail.com>
2023-03-06 08:51:08 +0900

    uploaded at 2023. 3. 6. 오전 8:51:08

---
+++ programmers/문자열 나누기/solution.java
@@ -0,0 +1,28 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/140108
+
+class Solution {
+    public int solution(String s) {
+        int answer = 1;
+
+        char start = s.charAt(0);
+        int startCount = 1;
+        int noneCount = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (startCount == noneCount) {
+                startCount = 1;
+                noneCount = 0;
+                start = s.charAt(i);
+                answer++;
+                continue;
+            }
+            if (start == s.charAt(i)) {
+                startCount++;
+            } else {
+                noneCount++;
+            }
+        }
+
+//        System.out.println(answer);
+        return answer;
+    }
+}
