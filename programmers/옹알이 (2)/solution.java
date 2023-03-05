e92a8d73a72055f8048d72d81d93799d97b49b50
LeeRim <yrim113@gmail.com>
2023-03-06 00:15:13 +0900

    uploaded at 2023. 3. 6. 오전 12:15:13

---
+++ programmers/옹알이 (2)/solution.java
@@ -0,0 +1,27 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/133499
+
+class Solution {
+    public int solution(String[] babbling) {
+        String[] ables = {"aya", "ye", "woo", "ma"};
+
+        int answer = 0;
+        for (String bab : babbling) {
+            String pre = "";
+            for (int i = 0; i < ables.length; i++) {
+                String able = ables[i];
+                if (bab.startsWith(able) && !pre.equals(able)) {
+                    bab = bab.substring(able.length());
+                    if (bab.length() == 0) {
+                        answer++;
+                        break;
+                    }
+                    pre = able;
+                    i = -1;
+                }
+            }
+        }
+
+//        System.out.println(answer);
+        return answer;
+    }
+}
