0bd0a55391ea680eff3f2a3cb53ea1b58dce9522
LeeRim <yrim113@gmail.com>
2023-03-07 17:10:07 +0900

    uploaded at 2023. 3. 7. 오후 5:10:07

---
+++ programmers/다음 큰 숫자/solution.java
@@ -0,0 +1,31 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12911
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String binaryN = Integer.toBinaryString(n);
+        int count = 0;
+        for (int i = 0; i < binaryN.length(); i++) {
+            if (binaryN.charAt(i) == '1') {
+                count++;
+            }
+        }
+        
+        int countNext = 0;
+        int next = n;
+        String binaryNext = "";
+        while (count != countNext) {
+            countNext = 0;
+            next++;
+            binaryNext = Integer.toBinaryString(next);
+            for (int i = 0; i < binaryNext.length(); i++) {
+                if (binaryNext.charAt(i) == '1') {
+                    countNext++;
+                }
+            }
+        }
+        
+        answer = next;
+        return answer;
+    }
+}
