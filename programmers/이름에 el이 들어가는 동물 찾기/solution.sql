e1e287675ead7e09aaf66b82e3e4ef69f3bc175d
LeeRim <yrim113@gmail.com>
2023-03-07 16:04:28 +0900

    updated at 2023. 3. 7. 오후 4:04:27

--- programmers/이름에 el이 들어가는 동물 찾기/solution.sql
+++ programmers/이름에 el이 들어가는 동물 찾기/solution.sql
@@ -1,7 +1,7 @@
-// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/59047
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/59047?language=oracle
 
 select ANIMAL_ID, NAME
 from ANIMAL_INS
-where NAME like '%el%'
+where lower(NAME) like '%el%'
     and ANIMAL_TYPE like 'Dog'
 order by NAME
