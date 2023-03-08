583b49493948d5eb294a6b05787c7472885bd4e2
LeeRim <yrim113@gmail.com>
2023-03-08 11:03:42 +0900

    uploaded at 2023. 3. 8. 오전 11:03:42

---
+++ programmers/구명보트/solution.java
@@ -0,0 +1,32 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42885
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+
+        Deque<Integer> weights = new ArrayDeque<>();
+        for (int person : people) {
+            weights.add(person);
+        }
+
+        int count = 0;
+        int first = weights.getFirst();
+        int last;
+        while (!weights.isEmpty()) {
+            count++;
+            last = weights.pollLast();
+            if (last + first <= limit) {
+                weights.poll();
+                if (!weights.isEmpty()) {
+                    first = weights.getFirst();
+                }
+            }
+        }
+
+        return count;
+    }
+}
