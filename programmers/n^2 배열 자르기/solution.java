ac6c1f0a11489307510e10bfd6f98ff6450ab988
LeeRim <yrim113@gmail.com>
2023-03-09 13:51:38 +0900

    uploaded at 2023. 3. 9. 오후 1:51:37

---
+++ programmers/n^2 배열 자르기/solution.java
@@ -0,0 +1,30 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/87390
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int n, long left, long right) {
+
+        int leftx = (int) (left / n);
+        int lefty = (int) (left % n);
+        int rightx = (int) (right / n);
+        int righty = (int) (right % n);
+        
+        List<Integer> list = new ArrayList<>();
+        for (int i = leftx; i <= rightx; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == rightx && j > righty) {
+                    break;
+                }
+                if (i == leftx && j < lefty) {
+                    continue;
+                }
+                list.add(Math.max(i + 1, j + 1));
+            }
+        }
+        
+//        System.out.println(list);
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
