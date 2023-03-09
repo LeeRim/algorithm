48b1ac86d015f7457f4575f92d3e2632a72090fe
LeeRim <yrim113@gmail.com>
2023-03-09 12:04:53 +0900

    uploaded at 2023. 3. 9. 오후 12:04:53

---
+++ programmers/위장/solution.java
@@ -0,0 +1,30 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42578
+
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> type = new HashMap<>();
+
+        String clothesKey = "";
+        int count = 0;
+        for (int i = 0; i < clothes.length; i++) {
+            clothesKey = clothes[i][1];
+            if (type.containsKey(clothesKey)) {
+                count = type.get(clothesKey);
+                type.put(clothesKey, count + 1);
+            } else {
+                type.put(clothesKey, 1);
+            }
+        }
+
+        count = 1;
+        for (Integer value : type.values()) {
+            count *= (value + 1);
+        }
+
+        int answer = count - 1;
+
+        return answer;
+    }
+}
