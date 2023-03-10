0105624c52aa91a1946bdb890b27e6c3633ebfa1
LeeRim <yrim113@gmail.com>
2023-03-10 14:00:15 +0900

    uploaded at 2023. 3. 10. 오후 2:00:15

---
+++ programmers/보호소에서 중성화한 동물/solution.sql
@@ -0,0 +1,7 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/59045
+
+select i.ANIMAL_ID, i.ANIMAL_TYPE, i.NAME
+from ANIMAL_INS i
+join ANIMAL_OUTS o on i.ANIMAL_ID = o.ANIMAL_ID
+where i.SEX_UPON_INTAKE like 'Intact%'
+    and o.SEX_UPON_OUTCOME not like 'Intact%'
