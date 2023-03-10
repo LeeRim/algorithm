da327e4bf6628bfea86078d093ed161e55bd5a8f
LeeRim <yrim113@gmail.com>
2023-03-10 14:47:25 +0900

    uploaded at 2023. 3. 10. 오후 2:47:24

---
+++ programmers/프린터/solution.java
@@ -0,0 +1,49 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42587
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        int myLocation = location;
+
+        Queue<Integer> orders = new LinkedList<>();
+        PriorityQueue<Integer> printOrders = new PriorityQueue<>(Collections.reverseOrder());
+        for (int priority : priorities) {
+            int p = Integer.valueOf(priority);
+            orders.add(p);
+            printOrders.add(p);
+        }
+
+        while (!orders.isEmpty()) {
+            int first = orders.poll();
+//            System.out.println("now : " + first);
+//            System.out.println("max : " + printOrders.peek());
+            if (first == printOrders.peek()) {
+                answer++;
+                printOrders.remove();
+                if (myLocation == 0) {
+                    break;
+                } else {
+                    myLocation--;
+                }
+            } else {
+                orders.add(first);
+                if (myLocation == 0) {
+                    myLocation = orders.size() - 1;
+                } else {
+                    myLocation--;
+                }
+            }
+//            System.out.println("me : " + myLocation);
+//            System.out.println("!asr : " + answer);
+        }
+
+//        System.out.println(answer);
+
+        return answer;
+    }
+}
