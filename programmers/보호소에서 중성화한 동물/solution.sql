6aa70b21ebc333d8f6ef727f565bb2869f1363b1
LeeRim <yrim113@gmail.com>
2023-03-10 14:01:36 +0900

    updated at 2023. 3. 10. 오후 2:01:36

--- programmers/보호소에서 중성화한 동물/solution.sql
+++ programmers/보호소에서 중성화한 동물/solution.sql
@@ -2,6 +2,9 @@
 
 select i.ANIMAL_ID, i.ANIMAL_TYPE, i.NAME
 from ANIMAL_INS i
-join ANIMAL_OUTS o on i.ANIMAL_ID = o.ANIMAL_ID
 where i.SEX_UPON_INTAKE like 'Intact%'
-    and o.SEX_UPON_OUTCOME not like 'Intact%'
+    and i.ANIMAL_ID in (
+        select o.ANIMAL_ID
+        from ANIMAL_OUTS o
+        where o.SEX_UPON_OUTCOME not like 'Intact%'
+    )
