a7a6b090b914b862a35d4cca7b0a19f575fb74c0
LeeRim <yrim113@gmail.com>
2023-03-10 15:40:02 +0900

    uploaded at 2023. 3. 10. 오후 3:40:02

---
+++ programmers/전화번호 목록/solution.java
@@ -0,0 +1,19 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42577
+
+import java.util.Arrays;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Arrays.sort(phone_book);
+//        System.out.println(Arrays.toString(phone_book));
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                answer = false;
+                break;
+            }
+        }
+        return answer;
+    }
+}
