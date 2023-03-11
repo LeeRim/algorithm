fcc67122aed981a620d3e7ce4f51e6d97bb39c6f
LeeRim <yrim113@gmail.com>
2023-03-11 14:27:47 +0900

    uploaded at 2023. 3. 11. 오후 2:27:47

---
+++ programmers/더 맵게/solution.java
@@ -0,0 +1,30 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42626
+
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+
+        PriorityQueue<Integer> scovilles = new PriorityQueue<Integer>();
+        for (int s : scoville) {
+            scovilles.add(Integer.valueOf(s));
+        }
+
+        int newsco = 0;
+        while (scovilles.size() > 1) {
+            if (scovilles.peek() < K) {
+                newsco = scovilles.poll() + (scovilles.poll() * 2);
+                if (newsco < K && scovilles.isEmpty()) {
+                    answer = -1;
+                    break;
+                }
+                answer++;
+                scovilles.add(newsco);
+            } else {
+                break;
+            }
+        }
+        return answer;
+    }
+}
