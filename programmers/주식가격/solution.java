975b74fcf1de45a3c0093119e60f1085d67af73e
LeeRim <yrim113@gmail.com>
2023-03-14 13:44:15 +0900

    updated at 2023. 3. 14. 오후 1:44:15

--- programmers/주식가격/solution.java
+++ programmers/주식가격/solution.java
@@ -1,19 +1,15 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42584
 
 class Solution {
-
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
 
-        for (int i = 0; i < prices.length - 1; i++) {
+        int[] answer = new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
             for (int j = i + 1; j < prices.length; j++) {
+                answer[i]++;
                 if (prices[i] > prices[j]) {
-                    answer[i] = j - i;
                     break;
                 }
-            }
-            if (answer[i] == 0) {
-                answer[i] = prices.length - 1 - i;
             }
         }
 
