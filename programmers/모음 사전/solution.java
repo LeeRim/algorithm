66e410a03a332429f7d89cce0213151f339354b6
LeeRim <yrim113@gmail.com>
2023-03-15 15:35:27 +0900

    uploaded at 2023. 3. 15. 오후 3:35:26

---
+++ programmers/모음 사전/solution.java
@@ -0,0 +1,30 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/84512
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    List<String> words;
+    public int solution(String word) {
+        words = new ArrayList<>();
+        String[] vowels = {"A", "E", "I", "O", "U"};
+        makeWord(vowels, new StringBuilder(), 0);
+        Collections.sort(words);
+        return words.indexOf(word);
+    }
+
+    public void makeWord(String[] vowels, StringBuilder sb, int count) {
+        if (count > 5) {
+            return;
+        }
+        words.add(sb.toString());
+        for (String vowel : vowels) {
+            sb.append(vowel);
+            makeWord(vowels, new StringBuilder(sb), count + 1);
+            if (!vowel.equals("")) {
+                sb.delete(sb.length() - 1, sb.length());
+            }
+        }
+    }
+}
