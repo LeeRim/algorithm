532abb064311657789fd9abbe63129fec1fbe15b
LeeRim <yrim113@gmail.com>
2023-03-16 18:04:22 +0900

    uploaded at 2023. 3. 16. 오후 6:04:22

---
+++ programmers/큰 수 만들기/solution.java
@@ -0,0 +1,23 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42883
+
+class Solution {
+    public String solution(String number, int k) {
+        String answer = "";
+        int n = 0;
+        int i = 0;
+        String str = "";
+        String max = "";
+        while (n < number.length() - k) {
+            n++;
+//            System.out.println("i : " + i + ", n : " + (k+n));
+            str = number.substring(i, k + n);
+//            System.out.println(str);
+            max = String.valueOf(Character.getNumericValue(str.chars().max().getAsInt()));
+//            System.out.println(max);
+            answer += max;
+            i += str.indexOf(max) + 1;
+        }
+//            System.out.println(answer);
+        return answer;
+    }
+}
