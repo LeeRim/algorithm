5e054a8977e93fdaaaafd8da86d16282f147fca2
LeeRim <yrim113@gmail.com>
2023-03-16 15:50:04 +0900

    uploaded at 2023. 3. 16. 오후 3:50:03

---
+++ programmers/가장 큰 수/solution.java
@@ -0,0 +1,27 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42746
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] strs = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            strs[i] = String.valueOf(numbers[i]);
+        }
+        Arrays.sort(strs, (a, b) -> (b + a).compareTo(a + b));
+//        System.out.println(Arrays.toString(strs));
+
+        String answer = "";
+        if (answer.startsWith("0")) {
+            answer = "0";
+        }
+        for (int i = 0; i < strs.length; i++) {
+            if (answer.equals("0")) {
+                break;
+            }
+            answer += strs[i];
+        }
+        return answer;
+    }
+}
