52d94155b95ae0f85045ded825ec8930eef232f3
LeeRim <yrim113@gmail.com>
2023-03-17 14:55:14 +0900

    updated at 2023. 3. 17. 오후 2:55:14

--- programmers/가장 큰 수/solution.java
+++ programmers/가장 큰 수/solution.java
@@ -9,12 +9,10 @@ class Solution {
         for (int number : numbers) {
             strs.add(String.valueOf(number));
         }
+        
+        strs.sort(((o1, o2) -> (o2 + o1).compareTo(o1 + o2)));
 
-        strs.sort((o1, o2) -> {
-            return (o2 + o1).compareTo(o1 + o2);
-        });
-
-        if(strs.get(0).startsWith("0")){
+        if (strs.get(0).charAt(0) == '0') {
             return "0";
         }
         StringBuilder sb = new StringBuilder();
