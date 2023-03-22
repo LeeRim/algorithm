352831b83844ee0295a20fdbed6ff97066eaa4d4
LeeRim <yrim113@gmail.com>
2023-03-22 17:39:26 +0900

    uploaded at 2023. 3. 22. 오후 5:39:26

---
+++ programmers/숫자 변환하기/solution.java
@@ -0,0 +1,36 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/154538
+
+class Solution {
+    public int solution(int x, int y, int n) {
+        int[] dp = new int[y + 1];
+
+        for (int i = x + 1; i <= y; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+
+        int next;
+        for (int i = x; i < y; i++) {
+            if (dp[i] == Integer.MAX_VALUE) {
+                continue;
+            }
+
+            for (int j = 3; j > 0; j--) {
+                if (j == 1) {
+                    next = i + n;
+                } else {
+                    next = i * j;
+                }
+
+                if (next > y) {
+                    continue;
+                }
+                dp[next] = Math.min(dp[next], dp[i] + 1);
+            }
+        }
+
+        if (dp[y] == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return dp[y];
+    }
+}
