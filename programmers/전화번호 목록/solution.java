3c51e7fcf10e1463a0c0e308b32a9bcb7e2a17ea
LeeRim <yrim113@gmail.com>
2023-03-22 16:13:50 +0900

    updated at 2023. 3. 22. 오후 4:13:49

--- programmers/전화번호 목록/solution.java
+++ programmers/전화번호 목록/solution.java
@@ -5,15 +5,16 @@ import java.util.Arrays;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        Arrays.sort(phone_book);
-//        System.out.println(Arrays.toString(phone_book));
 
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
+        Arrays.sort(phone_book);
+
+        for (int i = 1; i < phone_book.length; i++) {
+            if (phone_book[i].startsWith(phone_book[i - 1])) {
                 answer = false;
                 break;
             }
         }
+
         return answer;
     }
 }
