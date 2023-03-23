e9ec7a31b619ff52ea004a515bd2a69ed953fa95
LeeRim <yrim113@gmail.com>
2023-03-23 18:25:52 +0900

    uploaded at 2023. 3. 23. 오후 6:25:51

---
+++ programmers/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/solution.sql
@@ -0,0 +1,19 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/157339
+
+select c.CAR_ID, CAR_TYPE, FEE
+from CAR_RENTAL_COMPANY_CAR c
+left join (
+    select CAR_ID, FLOOR(daily_fee * (100 - DISCOUNT_RATE) * 0.3) as FEE
+    from CAR_RENTAL_COMPANY_CAR c
+    left join CAR_RENTAL_COMPANY_DISCOUNT_PLAN d on c.CAR_TYPE = d.CAR_TYPE
+    where DURATION_TYPE = '30일 이상'
+) as f on c.CAR_ID = f.CAR_ID
+where c.CAR_ID not in (
+    select CAR_ID
+    from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+    where (END_DATE >= DATE('2022-11-01') and END_DATE <= DATE('2022-11-30'))
+    or (START_DATE >= DATE('2022-11-01') and START_DATE <= DATE('2022-11-30'))
+    or (START_DATE <= DATE('2022-11-01') and END_DATE >= DATE('2022-11-30'))
+)
+and FEE >= 500000 and FEE < 2000000
+order by FEE DESC, CAR_TYPE, c.CAR_ID DESC
