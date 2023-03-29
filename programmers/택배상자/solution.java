885591ab59bd16a19232eff4935e1d16e6563d92
LeeRim <yrim113@gmail.com>
2023-03-29 10:42:22 +0900

    uploaded at 2023. 3. 29. 오전 10:42:22

---
+++ programmers/택배상자/solution.java
@@ -0,0 +1,24 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/131704
+
+import java.util.Stack;
+
+class Solution {
+    public int solution(int[] order) {
+        int cnt = 0;
+
+        Stack<Integer> 보조 = new Stack<>();
+
+        for(int i=0; i<order.length; i++){
+            보조.add(i+1);
+
+            while(!보조.isEmpty()){
+                if(보조.peek() == order[cnt]){
+                    보조.pop();
+                    cnt++;
+                }else break;
+
+            }
+        }
+        return cnt;
+    }
+}
