46fd53c5d53069394494ee7c80e9537de15114ea
LeeRim <yrim113@gmail.com>
2024-01-31 13:48:42 +0900

    updated at 2024. 1. 31. 오후 1:48:42

--- programmers/최댓값과 최솟값/solution.java
+++ programmers/최댓값과 최솟값/solution.java
@@ -6,12 +6,13 @@ class Solution {
     public String solution(String s) {
         int[] ints = Arrays.stream(s.split(" "))
                 .mapToInt(Integer::valueOf)
+                .sorted()
                 .toArray();
 
         StringBuilder sb = new StringBuilder();
-        sb.append(Arrays.stream(ints).min().getAsInt());
+        sb.append(ints[0]);
         sb.append(" ");
-        sb.append(Arrays.stream(ints).max().getAsInt());
+        sb.append(ints[ints.length - 1]);
 
         return sb.toString();
     }
