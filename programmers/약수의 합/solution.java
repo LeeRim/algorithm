b23b71b1257b5debe949a4360cb4d69d43b2522d
LeeRim <yrim113@gmail.com>
2024-02-01 15:22:48 +0900

    updated at 2024. 2. 1. 오후 3:22:47

--- programmers/약수의 합/solution.java
+++ programmers/약수의 합/solution.java
@@ -2,15 +2,21 @@
 
 class Solution {
     public int solution(int n) {
-        if(n < 2){
+        if (n < 2) {
             return n;
         }
+
         int answer = n + 1;
-        for (int i = 2; i <= n / 2; i++) {
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 answer += i;
+                if (i != Math.sqrt(n)) {
+                    answer += n / i;
+                }
             }
         }
+
         return answer;
     }
 }
